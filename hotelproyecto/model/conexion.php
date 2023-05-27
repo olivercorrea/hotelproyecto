87be99cb1ef25a5897a156a6e1dc4af7e15ae2d9
olivercorrea <116691853+olivercorrea@users.noreply.github.com>
2023-05-27 09:58:36 -0500

    Update conexion.php
    intentando una nueva conexion

--- hotelproyecto/model/conexion.php
+++ hotelproyecto/model/conexion.php
@@ -1,19 +1,18 @@
-<?php 
+<?php
 $contrasena = "AVNS_kKSkflCp2xGgajXHnqB";
 $usuario = "doadmin";
 $nombre_bd = "defaultdb";
+$host = "db-mysql-nyc1-28475-do-user-14089120-0.b.db.ondigitalocean.com";
+$port = 25060;
 
 try {
-	$bd = new PDO (
-		'mysql:host=db-mysql-nyc1-28475-do-user-14089120-0.b.db.ondigitalocean.com;
-		dbname='.$nombre_bd,
-		$usuario,
-		$contrasena,
-		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-	);
-	// Cierra la conexión a la base de datos
-	$bd = null;
+    $bd = new PDO(
+        'mysql:host='.$host.';port='.$port.';dbname='.$nombre_bd,
+        $usuario,
+        $contrasena,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+    );
 } catch (Exception $e) {
-	echo "Problema con la conexion: ".$e->getMessage();
+    echo "Problema con la conexión: " . $e->getMessage();
 }
 ?>
